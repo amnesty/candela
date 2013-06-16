9bcf5934aaaa13975838fd027c8d6005dad1c51c
Alvaro García <alvaro.garcia@gnoxys.net>
2013-06-16 18:16:13 +0200

    removed legacy routing for public controller, setting all routes explicitly

--- config/routes.rb
+++ config/routes.rb
@@ -173,12 +173,6 @@ AiVoluntariado::Application.routes.draw do
 
   #ROUTES FOR EVENT RECORDS
 
-#  resources :event_records do
-#    get :delete, :on => :member    
-#    resources :notes do
-#      get :delete, :on => :member    
-#    end
-#  end
   EventRecord.child_classes.prepend('EventRecord').each do |klass_name|
     resources klass_name.underscore.pluralize, :controller => 'event_records', :defaults => (klass_name != 'EventRecord' ? {:type => klass_name}:{}) do 
       get :delete, :on => :member    
@@ -204,12 +198,7 @@ AiVoluntariado::Application.routes.draw do
   match '/collaboration_types_for_search' => "search#collaboration_types_for",       :as => :collaboration_types_for_search
   match '/autonomic_teams_for_autonomy'   => "autonomies#autonomic_teams_for",       :as => :autonomic_teams_for_autonomy
   
-  match '/provinces_for_cps'              => "public#provinces_for", :as => :provinces_for_cps
-  match '/cities_for_province'            => "public#cities_for",    :as => :cities_for_province
-  match '/city_for_cp'                    => "public#cities_for",    :as => :city_for_cp
-  match '/cp_for_city'                    => "public#cp_for",        :as => :cp_for_city
-
-  #routes for auto_complete
+  # Routes for auto_complete
   #TODO: simplify these routes
   match '/auto_complete/talks_as_options_for_interesteds' => 'talks#talks_as_options_for_interesteds', :as => :talks_as_options_for_interesteds_talks
   match '/auto_complete/talks_as_options_for_search'      => 'talks#talks_as_options_for_search',      :as => :talks_as_options_for_search_talks
@@ -220,40 +209,38 @@ AiVoluntariado::Application.routes.draw do
 #  match '/auto_complete/countries_as_options_for_activists_collaborations/:country_id' => 'activists_collaborations#auto_complete_for_activist_search', :as => :auto_complete_for_activist_search_country_activists_collaborations
 #  match '/auto_complete/autonomies_as_options_for_activists_collaborations/:autonomy_id' => 'activists_collaborations#auto_complete_for_activist_search', :as => :auto_complete_for_activist_search_autonomy_activists_collaborations
 
-  # Routes for public controller: all actions accessible via GET requests.
-scope "(:locale)" do
-  match 'public/:action(.:format)', :controller => 'public'
-end
+  #PUBLIC ROUTES
 
-  # Other static routes  
-scope "(:locale)" do
-  match '/form_hr_schools'   => 'public#new_hr_school',  :as => :form_hr_schools
-end
+  # HHRR schools: localized actions
+  scope "(:locale)" do
+    match '/form_hr_schools'   => 'public#new_hr_school',  :as => :form_hr_schools
+    ["new_hr_school", "create_hr_school", "confirm_hr_school"].each do |action_name|
+      match "public/#{action_name}" => "public##{action_name}"
+    end
+  end
+
+  # Interesteds: non-localized actions
   match '/form_voluntariado' => 'public#new_interested', :as => :form_voluntariado
+  ["new_interested", "create_interested", "confirm_interested", "different_residence_country"].each do |action_name|
+    match "public/#{action_name}" => "public##{action_name}"
+  end
+
+  # Ajax request for forms
+  match '/provinces_for_cps'              => "public#provinces_for", :as => :provinces_for_cps
+  match '/cities_for_province'            => "public#cities_for",    :as => :cities_for_province
+  match '/city_for_cp'                    => "public#cities_for",    :as => :city_for_cp
+  match '/cp_for_city'                    => "public#cp_for",        :as => :cp_for_city
+
+  # Other static routes
   match '/gen_alerts'        => 'alerts#generate',  :as => :gen_alerts
   match '/search'            => 'search#index',     :as => :search
   match '/admin'             => 'admin#index',      :as => :admin
 
-#-----------------------------------------------------
-#TODO: remove STUB ROUTES (when controllers are added)
-
-#  match "/admin" => 'home#index', :as => :admin
-#  match "/search" => 'home#index', :as => :search
-#  match "/talks" => 'home#index', :as => :talks
-#  match "/campaignactions" => 'home#index', :as => :campaignactions
-#  match "/interesteds" => 'home#index', :as => :interesteds
-#  match "/hr_schools" => 'home#index', :as => :hr_schools
-#  match "/local_organizations" => 'home#index', :as => :local_organizations
-#  match "/se_teams" => 'home#index', :as => :se_teams
-#  match "/countries" => 'home#index', :as => :countries
-#  match "/autonomies" => 'home#index', :as => :autonomies
-#  match "/committees" => 'home#index', :as => :committees
-
   root :to => "home#index"
 
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  #TODO: old app had a similar wild controller route... uncommment if necessary
-#  match ':controller(/:action(/:id))(.:format)' # old routing: -->  map.default '/:controller/:action/:id', :controller => :controller, :action => :action
+  # This is a legacy wild controller route that's not recommended for RESTful applications.
+  # Uncommment only for route testing purposes
+#  match ':controller(/:action(/:id))(.:format)' 
 
 end
