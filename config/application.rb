8814c0e077194a9e424a32aeba747da7315aeb2c
Alvaro García <alvaro.garcia@gnoxys.net>
2013-07-17 11:14:48 +0200

    removed english as available locale for I18n

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module AiVoluntariado
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.available_locales = [:es, :ca, :eu, :gl, :en]
+    config.i18n.available_locales = [:es, :ca, :eu, :gl]
     config.i18n.default_locale = :es
     config.i18n.fallbacks = true
 
