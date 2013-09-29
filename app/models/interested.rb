a14ef7201054fc8dc1a0222aca799bd7e032bf38
Alvaro García <alvaro.garcia@gnoxys.net>
2013-09-29 20:34:15 +0200

    Added permission to create interesteds if user has create permission, no matter the stage

--- app/models/interested.rb
+++ app/models/interested.rb
@@ -56,6 +56,13 @@ class Interested < ActiveRecord::Base
 
   scope :with_talks, lambda { |q| q.blank? ? {} : (Gx.to_boolean(q) ? {:joins => :talks} : {:conditions => 'interesteds.id NOT IN (SELECT DISTINCT(interested_id) FROM interesteds_talks)' }) }
 
+  # Permission to create interested if user has any permission to create
+  authorizing do |user, permission|
+    if user.is_a?(User) and permission == :create
+      user.has_any_permission_to(:create, :interested)
+    end || nil
+  end 
+
   # Last authorization block. Interested can be CRUD by user if user has role on its local_organization
   authorizing do |user, permission|
     if user.is_a?(User) and user.has_any_permission_to(permission, :interested)
