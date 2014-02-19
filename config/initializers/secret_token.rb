c11718d8592cab62eb49cb08a2eb41f9124d3561
Alvaro García <alvaro.garcia@gnoxys.net>
2014-02-19 20:22:09 +0100

    added field to app_settings

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-AiVoluntariado::Application.config.secret_token = '67a61b44299ed4fae2cd0187335d9f58b04f264702a75410a925a338855980d2674f715edf6b4769ae0f1a37af45d18e5e430a2c3fe151dc57b4d020641ff512'
+AiVoluntariado::Application.config.secret_token = Settings.secret_token
+
