890a1206fb4572ec6f8406f25b836212ecfc85e5
alvarito <agarcia@anasoft.es>
2015-09-25 12:54:50 +0200

    Responsive navigation menu with touch controls

---
+++ app/assets/javascripts/navigation_bar.js
@@ -0,0 +1,44 @@
+
+function onWindowResize() {
+//alert('onWindowResize');  
+  var $nav = $('#main_nav_menu');
+  var breakpoint = $nav.data('breakpoint');
+  if ($(window).width() <= breakpoint) {
+    if (!$nav.hasClass("sm-screen")) {
+      $('#main_nav_menu').addClass('collapsed');  
+      $('#main_nav_menu_toggler').addClass('collapsed');  
+    }
+    $nav.removeClass("lg-screen").addClass("sm-screen");
+  } else {
+    if (!$nav.hasClass("lg-screen")) {
+      $('#main_nav_menu').removeClass('collapsed');  
+      $('#main_nav_menu_toggler').removeClass('collapsed');  
+    }
+    $nav.removeClass("sm-screen").addClass("lg-screen");
+  }
+  $('#main_nav_menu li').removeClass('active');
+}
+
+$(document).on('click', '#main_nav_menu_toggler', function(e) {
+  e.preventDefault();
+  $('#main_nav_menu').toggleClass('collapsed');  
+  $('#main_nav_menu_toggler').toggleClass('collapsed');  
+});
+
+$(document).on('click', '#main_nav_menu li a, #main_nav_menu li .touch-button', function(e) {
+  e.preventDefault();
+  var $parent = $(this).closest('li');
+  $parent.siblings('li').removeClass('active');
+  $parent.toggleClass('active');
+});
+    
+$(window).on('resize', onWindowResize);
+
+//$(window).on('resize', function(){alert('callback #1');});
+//$(window).on('resize', function(){alert('callback #2');});
+
+
+$( document ).ready(function( $ ) {
+  onWindowResize();
+});
+
