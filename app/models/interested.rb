b3ace0897fca793a2ae5c4fb008d846f0060a40c
alvarito <agarcia@anasoft.es>
2015-09-28 14:59:16 +0200

    Exclude interesteds with cleared data from lists

--- app/models/interested.rb
+++ app/models/interested.rb
@@ -247,8 +247,9 @@ class Interested < ActiveRecord::Base
   # Show only the interesteds of the group logged in
   scope :can_see, lambda { |agent|
     if agent.is_a?(User) and agent.has_any_permission_to(:read, :interested)
+      res_scope = has_cleared_sensitive_data(false)
       lloo_ids = agent.performances.where(:stage_type => 'LocalOrganization').pluck(:stage_id)
-      lloo_ids.any? ? {:conditions => {:local_organization_id => lloo_ids}} : {}
+      lloo_ids.any? ? res_scope.where(:local_organization_id => lloo_ids) : res_scope
     else
       where('1=0')
     end
